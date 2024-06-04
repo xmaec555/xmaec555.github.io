e176af584223e6883fcead086370afb37652b381
xmaec555 <63795414+xmaec555@users.noreply.github.com>
2024-06-04 13:11:07 +0800

    Add files via upload

--- js/mp4us.js
+++ js/mp4us.js
@@ -55,10 +55,10 @@ d.forEach(function(it) {
 	}
 });
 if (tabsm === true){
-	TABS.push("磁力");
+	TABS.push("关注公众号【洪溪208】");
 }
 if (tabse === true){
-	TABS.push("電驢");
+	TABS.push("电驴");
 }
 if (false && tabsa.length + tabsq.length > 1){
 	TABS.push("選擇右側綫路");
